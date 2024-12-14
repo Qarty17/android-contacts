af5c53d9e57f745e161417d27f51fe57404d3bca
Qarty17 <120599640+Qarty17@users.noreply.github.com>
2024-12-14 21:11:32 +0300

    Update ListDiffInterface.java

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface<T> {
-    boolean theSameAs(T o);
+    default boolean theSameAs(T o){
+        return true;
+    }
     boolean equals(Object o2);
 }
